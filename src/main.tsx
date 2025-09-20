28646fc475c5f9b4d3657a408840261ec2b2bab4
devnull <devnull@local>
2025-09-21 00:14:00 +0200

    Empezando a arreglar que muestre textos

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import { createRoot } from 'react-dom/client'
+﻿import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TranslationProvider } from '@/contexts/TranslationContext'
+
+import './App.css'
 
 createRoot(document.getElementById("root")!).render(
   <TranslationProvider>
