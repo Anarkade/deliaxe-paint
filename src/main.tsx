472c23a85fe9ba409e2c4e6b531441348acaa9cf
devnull <devnull@local>
2025-09-21 00:02:28 +0200

    APP RESUCITADA!

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TranslationProvider } from '@/contexts/TranslationContext'
-// Dev-only visual helpers (loaded only in dev mode)
-if (import.meta.env.DEV) {
-  import('./dev.css');
-}
 
 createRoot(document.getElementById("root")!).render(
   <TranslationProvider>
